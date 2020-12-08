89e63b3eb16b847610a957890a7368e97d29e8f8
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2020-12-07 21:40:49 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,13 @@
+namespace POO_heranca
+{
+    public class Pessoa
+    {
+        public string nome;
+
+        public string pouf;
+
+        public string DarBoasVindas(string nomeUsuario){
+            return "Seja bem vindo(a) ao nosso sistema, "+nomeUsuario;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace POO_heranca
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Instaciamos nossa subclasse
+            PessoaFisica pf = new PessoaFisica();
+
+            PessoaJuridica pj = new PessoaJuridica();
+            
+            Console.WriteLine("Este cadastro é pessoal ou empresarial? - p ou e");
+            pf.pouf = Console.ReadLine();
+
+            if(pf.pouf == "p"){
+            
+            //Atribímos um valor para a propiedade disponivel na superclasse
+            Console.WriteLine("Digite seu nome");
+            pf.nome = Console.ReadLine();
+            
+            Console.WriteLine("Digite seu CPF");
+            pf.cpf = Console.ReadLine();
+
+            //Mostramos no console o método de saudação da superclasse
+            Console.WriteLine(pf.DarBoasVindas(pf.nome) );
+            
+            Console.WriteLine(pf.ValidarCPF(pf.cpf) );
+            }
+            else if(pf.pouf == "e"){
+                Console.WriteLine("Digite o nome da empresa");
+            pf.nome = Console.ReadLine();
+            
+            Console.WriteLine("Digite o CNPJ");
+            pj.cnpj = Console.ReadLine();
+
+            Console.WriteLine("Digite a inscrição estadual");
+            pj.inscricaoEstadual = Console.ReadLine();
+
+            Console.WriteLine(pf.DarBoasVindas(pf.nome) );
+            
+            Console.WriteLine(pf.ValidarCPF(pf.cpf) );
+
+            }
+            
+
+
+            
+            
+        
+
+        }
+    }
+}
+                                                                                               
